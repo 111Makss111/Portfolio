f9d4932b529c0ee34b46559ca7dca34ef9597e2e
111Makss111 <151084313+111Makss111@users.noreply.github.com>
2026-02-07 02:33:57 +0100

    'update'

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+// Додаємо перевірку в консоль
+const mobileMenu = document.querySelector(".backdrop");
+const openMenuBtn = document.querySelector(".header-button");
+const closeMenuBtn = document.querySelector(".backdrop-close-btn");
+
+console.log("Open Button:", openMenuBtn); // Має вивести кнопку
+console.log("Close Button:", closeMenuBtn); // Якщо тут null - JS не бачить хрестик
+console.log("Menu:", mobileMenu); // Має вивести div
+
+if (openMenuBtn && closeMenuBtn && mobileMenu) {
+  openMenuBtn.addEventListener("click", () => {
+    mobileMenu.classList.add("is-open");
+  });
+
+  closeMenuBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("is-open");
+  });
+} else {
+  console.error("Один з елементів не знайдено! Перевір класи в HTML.");
+}
