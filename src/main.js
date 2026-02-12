3b7467a499d6da25cba31200b46b24ac8814ab51
111Makss111 <151084313+111Makss111@users.noreply.github.com>
2026-02-12 18:19:26 +0100

    'update'

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+// core version + navigation, pagination modules:
+import Swiper from "swiper";
+// Додатково імпортуємо модулі, які нам точно знадобляться
+import { Navigation, Pagination } from "swiper/modules";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// init Swiper:
+const swiper = new Swiper(".swiper", {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+});
+
 const mobileMenu = document.querySelector(".backdrop");
 const openMenuBtn = document.querySelector(".header-button");
 const closeMenuBtn = document.querySelector(".backdrop-close-btn");
